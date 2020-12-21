57467de215874271d77a81d056296a5a9457aca4
Rudolf866 <kovalevskiy2017@outlook.com>
2020-12-21 21:52:41 +0300

    createData

--- public_html/core/admin/controller/BaseAdmin.php
+++ public_html/core/admin/controller/BaseAdmin.php
@@ -16,6 +16,7 @@ abstract class BaseAdmin extends BaseController
 
     protected $table;
     protected $columns;
+    protected $data;
 
     protected $menu;
     protected $title;
@@ -60,6 +61,60 @@ abstract class BaseAdmin extends BaseController
 
         if($this->columns) new RouteException('Не найдены поля в таблице - '.$this->table,2);
 
+
+    }
+
+    protected function createData($arr = [], $add = true)
+    {
+        $fields = [];
+        $order = [];
+        $order_direction = [];
+
+        if($add){
+
+            if(!$this->columns['id_row']) return $this->data = [];
+            $fields[] = $this->columns['id_row'] . ' as id';
+
+            if($this->columns['name']) $fields['name'] = 'name';
+            if($this->columns['img']) $fields['img'] = 'img';
+
+            if(count($fields) < 3){
+
+                foreach($this->columns as $key => $item){
+
+                    if(!$fields['name'] && strpos($key,'name') !== false){
+
+                        $fields['name'] = $key . ' as name';
+                    }
+
+                    if(!$fields['img'] && strpos($key,'img') !== false){
+
+                        $fields['img'] = $key . ' as img';
+                    }
+
+                }
+            }
+
+            if($this->columns['parent_id']){
+
+                if(!in_array('parent_id',$fields)) $fields[] = 'parent_id';
+                $order[] = 'parent_id';
+
+            }
+
+            if($arr){
+                $fields = Settings::instance()->arrayMergeRecursive($fields,$arr['fields']);
+            }
+
+        }else{
+
+            if(!$arr) return $this->data = [];
+
+            $fields = $arr['fields'];
+            $order = $arr['order'];
+            $order_direction = ['order_direction'];
+
+        }
 
     }
 
